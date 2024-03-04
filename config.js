087ffc74f4d5deb5876490c9593e34793e686b20
Hieu05 <hieuhip.05052004@gmail.com>
2024-03-04 15:19:45 +0700

    innit project

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Vợ Yêu';
+const giftUrl = 'https://www.facebook.com/behieu2k4/';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng vợ yêu Của Anh';
+const contentCard = 'Chúc vợ yêu của anh 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em và anh trong hôm nay và cả những ngày sau. Anh rất xin lỗi vì không thể đi chơi hay tặng quà cho em ngày hôm nay anh sẽ cố gắng bù đắp lại cho em những ngày sau nhé mãi yêu vợ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Hiếu https://www.facebook.com/HieuFF.05/
+const giftImage = 'behien1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
