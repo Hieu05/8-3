d74613116db23eb33be99eb80f4752ef1938d38e
Hieu05 <hieuhip.05052004@gmail.com>
2024-03-05 10:47:33 +0700

    upload code

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-const nameGirl = 'Vợ Yêu';
-const giftUrl = 'https://www.facebook.com/behieu2k4/';
+const nameGirl = 'Bé iu';
+const giftUrl = 'https://www.facebook.com/huynhkyngan214/';
 const eventName = 'Chúc Mừng 08-03';
-const titleCard = 'Tặng vợ yêu Của Anh';
-const contentCard = 'Chúc vợ yêu của anh 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em và anh trong hôm nay và cả những ngày sau. Anh rất xin lỗi vì không thể đi chơi hay tặng quà cho em ngày hôm nay anh sẽ cố gắng bù đắp lại cho em những ngày sau nhé mãi yêu vợ';
+const titleCard = 'Tặng Bé iu Của chị';
+const contentCard = 'Chúc embe 8/3 duii dẻ nhooo. Cảm ơn em vì đã ở bên chị. Hy vọng là chúng ta sẽ mãi luôn như vậy. Chị thương bé';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Hiếu https://www.facebook.com/HieuFF.05/
-const giftImage = 'behien1.png';
+const giftImage = 'ngan.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
